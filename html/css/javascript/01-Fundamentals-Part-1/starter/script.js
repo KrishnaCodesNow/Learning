cbfe33b6ec2fdbafcd6473fc9fc89cc7c17cc83d
krishna tiwari <ktkrishna2024@gmail.com>
2025-11-22 00:08:12 +0530

    Adding part 1 fundamentals

--- html/css/javascript/01-Fundamentals-Part-1/starter/script.js
+++ html/css/javascript/01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,9 @@
  
-      let js ="amazing";
-      if (js === "amazing") alert("javascript is fun with krishna")
-      console.log(3+3)
+let js ="amazing";
+if (js === "amazing") alert("javascript is fun with krishna")
+console.log(3+3)
 
-      
+console.log("krishna")    
+
+
     

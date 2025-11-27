2f8e499bf36da3b7073b6bfbc592f19d616e73b0
krishna tiwari <ktkrishna2024@gmail.com>
2025-11-27 20:33:49 +0530

    assgnment operator

--- html/css/javascript/01-Fundamentals-Part-1/starter/script.js
+++ html/css/javascript/01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,39 @@
- 
+/*
 let js ="amazing";
 if (js === "amazing") alert("javascript is fun with krishna")
 console.log(3+3)
+let fristname = "krishna";
+console.log(fristname);
+*/
 
-console.log("krishna")    
+let javascriptisfun = true;
+console.log(javascriptisfun);
+
+console.log(typeof javascriptisfun);
+
+javascriptisfun = "yes!";
+console.log(typeof javascriptisfun);
+krishna = "krishna"
+console.log(krishna)
+
+//Assignment operator 
+let x = 10 + 5;
+x += 10;
+x +=4;
+x++;
+x--;
+console.log(x) 
 
 
-    
+
+
+
+
+   
+
+
+
+
+
+
+

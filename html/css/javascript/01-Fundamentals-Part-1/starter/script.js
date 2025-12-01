ee3176b8538cced1e5f03864f7eae395bcb00b5a
krishna tiwari <ktkrishna2024@gmail.com>
2025-12-01 23:06:21 +0530

    oprators done some

--- html/css/javascript/01-Fundamentals-Part-1/starter/script.js
+++ html/css/javascript/01-Fundamentals-Part-1/starter/script.js
@@ -4,7 +4,7 @@ if (js === "amazing") alert("javascript is fun with krishna")
 console.log(3+3)
 let fristname = "krishna";
 console.log(fristname);
-*/
+
 
 let javascriptisfun = true;
 console.log(javascriptisfun);
@@ -25,7 +25,34 @@ x--;
 console.log(x) 
 
 
+console.log(10 + 10);
+const ageJonas = 25;
+const ageSarah = 17;
 
+console.log(ageJonas > ageSarah); // true
+console.log(ageSarah >= 18);      // false
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+let x,y;
+x = y = 25 - 10 - 5;
+console.log(x,y);
+
+const fristname = "jonas";
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm" + fristname + ', a ' + (year - birthYear) + "year old " + job + "!";
+const jonass =   `I m  ${fristname} , a ${year - birthYear} , year old  ${job} !`;
+console.log(jonas)
+console.log(jonass)
 
 
 
